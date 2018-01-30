be687d4d34c567c798bf484559fdeecbe3f89788
Eduardo Pinho <enet4mikeenet@gmail.com>
2018-01-30 11:49:57 +0000

    Implement more IndexIVFFlat methods

--- c_api/IndexIVF_c.cpp
+++ c_api/IndexIVF_c.cpp
@@ -13,8 +13,10 @@
 #include "Clustering_c.h"
 #include "IndexIVF_c.h"
 #include "IndexIVF.h"
+#include "Index.h"
 #include "macros_impl.h"
 
+using faiss::Index;
 using faiss::IndexIVF;
 using faiss::IndexIVFFlat;
 using faiss::IndexIVFStats;
@@ -101,14 +103,32 @@ int faiss_IndexIVFFlat_new(FaissIndexIVFFlat** p_index) {
 }
 
 int faiss_IndexIVFFlat_new_with(FaissIndexIVFFlat** p_index,
-    FaissIndex* quantizer, size_t d, size_t nlist);
+    FaissIndex* quantizer, size_t d, size_t nlist)
+{
+    try {
+        auto q = reinterpret_cast<Index*>(quantizer);
+        *p_index = reinterpret_cast<FaissIndexIVFFlat*>(new IndexIVFFlat(q, d, nlist));
+    } CATCH_AND_HANDLE
+}
 
 int faiss_IndexIVFFlat_new_with_metric(
     FaissIndexIVFFlat** p_index, FaissIndex* quantizer, size_t d, size_t nlist,
-    FaissMetricType metric);
+    FaissMetricType metric)
+{
+    try {
+        auto q = reinterpret_cast<Index*>(quantizer);
+        auto m = static_cast<faiss::MetricType>(metric);
+        *p_index = reinterpret_cast<FaissIndexIVFFlat*>(new IndexIVFFlat(q, d, nlist, m));
+    } CATCH_AND_HANDLE
+}
 
 int faiss_IndexIVFFlat_add_core(FaissIndexIVFFlat* index, idx_t n, 
-    const float * x, const long *xids, const long *precomputed_idx);
+    const float * x, const long *xids, const long *precomputed_idx)
+{
+    try {
+        reinterpret_cast<IndexIVFFlat*>(index)->add_core(n, x, xids, precomputed_idx);
+    } CATCH_AND_HANDLE
+}
 
 /** Update a subset of vectors.
  *
@@ -119,4 +139,9 @@ int faiss_IndexIVFFlat_add_core(FaissIndexIVFFlat* index, idx_t n,
  * @param v      vectors of new values, size nv*d
  */
 int faiss_IndexIVFFlat_update_vectors(FaissIndexIVFFlat* index, int nv,
-    idx_t *idx, const float *v);
+    idx_t *idx, const float *v)
+{
+    try {
+        reinterpret_cast<IndexIVFFlat*>(index)->update_vectors(nv, idx, v);
+    } CATCH_AND_HANDLE
+}
