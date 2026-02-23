f0ad5ca8cf430c9c8a5b4ae091d2ea97d4473fd8
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2026-02-23 11:27:43 -0600

    Use initial current_segment of 0 and initialize buffers to 0

--- faiss/impl/AuxIndexStructures.cpp
+++ faiss/impl/AuxIndexStructures.cpp
@@ -80,9 +80,13 @@ RejectionResult::~RejectionResult() {
  ***********************************************************************/
 
 SegmentsResult::SegmentsResult(size_t num_segments)
-        : num_segments(num_segments), current_segment(1) {
+        : num_segments(num_segments), current_segment(0) {
     limits = new size_t[num_segments + 1];
     data = new uint16_t[65536 * num_segments];
+
+    memset(limits, 0, sizeof(*limits) * (num_segments + 1));
+    memset(data, 0, sizeof(*data) * (65536 * num_segments));
+
 }
 
 void SegmentsResult::add(uint16_t value) {
